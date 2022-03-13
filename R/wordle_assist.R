9cbc82ab3f01054d9941456be471c2752aace0e9
Robert McDonald <rmcd1024@gmail.com>
2022-03-12 21:47:29 -0600

    added wordle_assist.R

---
+++ R/wordle_assist.R
@@ -0,0 +1,63 @@
+#' @title wordle-assist
+#'
+#' @description \code{wordle-assist} returns 5-letter words that match
+#'     included and excluded letter patterns.
+#'
+#' @name wordle-assist
+#'
+#' @return Returns a vector of 5-letter words meeting supplied match
+#'     criteria
+#'
+#' @param no letters not in the word. These should be specified as a
+#'     character string, for example, \code{'aeontfd'}
+#'
+#' @param yes a list of letters included in the word, with positional
+#'     information. Correct positions are positive integers and
+#'     incorrect positions are negative integers. For example if "a"
+#'     is in the second position and not in the fourth, and "y" is in
+#'     the fifth, specify \code{yes = list(a = c(2, -4), y = 5)}
+#'
+#' @param words vector of admissible possible words. By default, uses
+#'     the length-5 words from the `words` package.
+#'
+#' @examples
+#'
+#' wordle_assist(no = 'seuntilmp', yes = list(a = -1, r = -2, o = -3, c = -1, h = -2))
+#'
+#'
+
+
+
+#' @export
+wordle_assist <- function(no = NULL, yes = NULL, words = words5) {
+    testmode <- FALSE
+    if (testmode) {
+        no <- 'abc'
+        yes <- list(d = c(1,  -2), y = c(-3, 5), e = -1, i = -1, t = -1)
+        i <- 2
+    }
+    if (length(no) >= 1) {
+        tmp <- grep(paste0('[', no, ']'), words, invert = TRUE, value = TRUE) 
+    } else {
+        tmp <- words
+    }
+    for (i in seq_along(yes)) {
+        grid <- rep('.', 5)
+        unknown_pos <- yes[[i]][yes[[i]]<0]
+        if (length(unknown_pos) > 0) {
+            ## all words that contain the letter
+            tmp <- grep(names(yes[i]), tmp, value = TRUE)
+            grid[-unknown_pos] <- paste0('[^', names(yes[i]), ']')
+            ##print(grid)
+            tmp <- grep(paste0(grid, collapse = ''), tmp, value = TRUE)
+        }
+        known_pos <- yes[[i]][yes[[i]]>0]
+        if (length(known_pos > 0)) {
+            grid[known_pos] <- names(yes[i])
+            tmp <- grep(paste0(grid, collapse = ''), tmp, value = TRUE)
+        }
+    }
+    tmp
+}
+
+
---
+++ R/words5.R
@@ -0,0 +1,6 @@
+#' All length-5 words in the `words` package
+#'
+#' @format vector of names
+#' @source \code{words5 <- words::words[words::words$word_length==5, 'word']}
+#'
+"words5"
