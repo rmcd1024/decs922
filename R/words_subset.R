4dcccd9486f861000f00222051a0870661830760
Robert McDonald <rmcd1024@gmail.com>
2022-07-03 21:47:37 -0500

    new function `words_subset`

---
+++ R/words_subset.R
@@ -0,0 +1,40 @@
+#' @title words-subset
+#'
+#' @description \code{words-subset} returns 5-letter words that
+#'     contain specified letters. Error if more than 5 letters are
+#'     supplied
+#'
+#' @name words-subset
+#'
+#' @return Returns a vector of 5-letter words meeting supplied match
+#'     criteria
+#'
+#' @param ltrs letters in the word. These should be specified as a
+#'     character string, for example, \code{'aeontfd'}
+#'
+#' @param words vector of admissible possible words. By default, uses
+#'     the length-5 words from the `words` package.
+#'
+#' @examples
+#'
+#' words_subset(ltrs = 'cawk')
+#'
+#'
+
+
+#' @export
+words_subset <- function(ltrs = NULL, words = words5) {
+    lltrs <- nchar(ltrs)
+    if (lltrs > 0 && lltrs < 6) {
+        tmp <- words5
+        ltrsv <- strsplit(ltrs, '')[[1]]
+        for (i in ltrsv) {
+            tmp <- grep(i, tmp, value = TRUE)
+        }
+    } else {
+        stop('Number of letters must be between 1 and 5, inclusive')
+    }
+    tmp
+}
+
+
